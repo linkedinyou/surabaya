d55970948125ca967ded9d396ab7409fa6b1e3d2
AkiraSonoda <akira.sonoda.1@gmail.com>
2012-12-29 11:52:05 +0100

    Caching of the Textures implemented

--- surabaya/surabaya-web/src/main/java/org/openjgrid/services/asset/AssetService.java
+++ surabaya/surabaya-web/src/main/java/org/openjgrid/services/asset/AssetService.java
@@ -1,8 +1,7 @@
 package org.openjgrid.services.asset;
 
-import java.util.UUID;
-
 import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 import javax.ejb.EJB;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
@@ -13,13 +12,9 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
-
 import org.openjgrid.datatypes.AssetBase;
-import org.openjgrid.datatypes.llsd.InventoryCollection;
 import org.openjgrid.services.infrastructure.ConfigurationService;
-import org.openjgrid.util.HexDump;
 import org.openjgrid.util.Util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,43 +27,56 @@ import org.slf4j.LoggerFactory;
 public class AssetService {
 	private static final Logger log = LoggerFactory.getLogger(AssetService.class);
 
+	@Resource(lookup = "java:jboss/surabaya_cache")
+	private org.infinispan.manager.CacheContainer container;
+	private org.infinispan.Cache<String, String> cache;
+
 	@EJB(mappedName = "java:module/ConfigurationService")
 	private ConfigurationService configuration;
-	
+
 	private HttpClient httpclient = null;
 
 	@PostConstruct
 	public void init() {
+		this.cache = this.container.getCache();
 		httpclient = new DefaultHttpClient();
 	}
 
 	public AssetBase getAsset(String assetID) {
 		log.debug("getAsset(assetID:{})", assetID);
+		String content = null;
 		AssetBase assetBase = new AssetBase();
 		try {
-			HttpGet httpget = new HttpGet(configuration.getProperty("grid",
-					"asset_service") + "/assets/" + assetID);
-    		HttpResponse httpResponse = httpclient.execute(httpget);
-        	HttpEntity entity = httpResponse.getEntity();
+			if (cache.containsKey(assetID)) {
+				log.debug("Cache Hit: {}", assetID);
+			} else {
+				log.debug("Cache Miss: {}", assetID);
 
-        	long contentLength = entity.getContentLength();
-        	Header contentType = entity.getContentType();
-        	String content = IOUtils.toString(entity.getContent());
-        	log.debug("ContentLength: {}", contentLength);
-        	log.debug("ContentType: {}", contentType);
-//        	log.debug("respstring: " + content);
-        	
-        	if(Util.isNullOrEmpty(content)) {
-        		return (null);
-        	}
-        	assetBase.fromXml(content);
-        	
-        	return(assetBase);
-			
+				HttpGet httpget = new HttpGet(configuration.getProperty("grid", "asset_service") + "/assets/" + assetID);
+				HttpResponse httpResponse = httpclient.execute(httpget);
+				HttpEntity entity = httpResponse.getEntity();
+
+				long contentLength = entity.getContentLength();
+				Header contentType = entity.getContentType();
+				content = IOUtils.toString(entity.getContent());
+				log.debug("ContentLength: {}", contentLength);
+				log.debug("ContentType: {}", contentType);
+				if(!Util.isNullOrEmpty(content)) {
+					cache.put(assetID, content);					
+				}
+			}
+			// log.debug("respstring: " + content);
+			if (Util.isNullOrEmpty(content)) {
+				return (null);
+			}
+			assetBase.fromXml(content);
+
+			return (assetBase);
+
 		} catch (Exception ex) {
 			log.error("Exception in AssetService", ex);
 		}
-		
+
 		return (assetBase);
 	}
 
